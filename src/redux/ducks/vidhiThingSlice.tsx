a344a2c2b542b30373ca7f43dd3796319859b5f2
Siddharth-Patel <pnmewada@gmail.com>
2023-06-21 19:24:57 +0530

    Css Change

---
+++ src/redux/ducks/vidhiThingSlice.tsx
@@ -0,0 +1,26 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {ItemData} from '../../models/itemList';
+
+const initialState: ItemData[] = [];
+
+const vidhilistCreateSlice = createSlice({
+  name: 'list',
+  initialState,
+  reducers: {
+    addList: (state, action) => {
+      const stateObj = Object.values(state);
+      stateObj.push(action.payload);
+      return stateObj;
+    },
+    delelteList: (state, action) => {
+      const {id} = action.payload;
+      const stateObj = [...Object.values(state)];
+      const removedData = stateObj.filter(list => list.listId !== id);
+      return removedData;
+    },
+  },
+});
+
+const {actions, reducer} = vidhilistCreateSlice;
+export const {addList, delelteList} = actions;
+export default reducer;
